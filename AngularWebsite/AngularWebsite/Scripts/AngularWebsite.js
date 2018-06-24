d45cc37d0805ec62f41ba639adcd1d375f6390c6
shaneocall <ocallshane@outlook.com>
2018-06-24 17:38:29 -0400

    Angular Tutorial P1 completed
    Angular  tutorial P1 finshed
    -Add Login
    -Add Register
    -Re-routing

--- AngularWebsite/AngularWebsite/Scripts/AngularWebsite.js
+++ AngularWebsite/AngularWebsite/Scripts/AngularWebsite.js
@@ -2,33 +2,34 @@
 
 AngularWebsite.controller('LandingPageController', LandingPageController);
 AngularWebsite.controller('LoginController', LoginController);
+AngularWebsite.controller('RegisterController', RegisterController);
 
 AngularWebsite.factory('AuthHttpResponseInterceptor', AuthHttpResponseInterceptor);
+AngularWebsite.factory('LoginFactory', LoginFactory);
+AngularWebsite.factory('RegistrationFactory', RegistrationFactory);
 
 var configFunction = function ($routeProvider, $httpProvider) {
     $routeProvider.
-        when('/routeOne',
-            {
-                templateUrl: 'routesDemo/one'
-            })
-        .when('/routeTwo/:donuts',
-            {
-                templateUrl: function(params) {return 'routesDemo/two?donuts=' + params.donuts;}
+        when('/routeOne', {
+            templateUrl: 'routesDemo/one'
         })
-        .when('/routeThree',
-            {
-                templateUrl: 'routesDemo/three'
-            })
-
-        .when('/login?returnUrl',
-            {
-                templateUrl: '/Account/login',
-                controller: LoginController
-            });
+        .when('/routeTwo/:donuts', {
+            templateUrl: function (params) { return '/routesDemo/two?donuts=' + params.donuts; }
+        })
+        .when('/routeThree', {
+            templateUrl: 'routesDemo/three'
+        })
+        .when('/login', {
+            templateUrl: '/Account/Login',
+            controller: LoginController
+        })
+        .when('/register', {
+            templateUrl: '/Account/Register',
+            controller: RegisterController
+        });
 
     $httpProvider.interceptors.push('AuthHttpResponseInterceptor');
 }
-
-configFunction.$inject = ['$routeProvider'];
+configFunction.$inject = ['$routeProvider', '$httpProvider'];
 
 AngularWebsite.config(configFunction);
--- AngularWebsite/AngularWebsite/Scripts/Controllers/LoginController.js
+++ AngularWebsite/AngularWebsite/Scripts/Controllers/LoginController.js
@@ -1,4 +1,4 @@
-﻿var LoginController = function ($scope, $routeParams) {
+﻿var LoginController = function ($scope, $routeParams, $location, LoginFactory) {
     $scope.loginForm = {
         emailAddress: '',
         password: '',
@@ -7,8 +7,19 @@
     };
 
     $scope.login = function () {
-        //todo
+        var result = LoginFactory($scope.loginForm.emailAddress, $scope.loginForm.password, $scope.loginForm.rememberMe);
+        result.then(function (result) {
+            if (result.success) {
+                if ($scope.loginForm.returnUrl !== undefined) {
+                    $location.path('/routeOne');
+                } else {
+                    $location.path($scope.loginForm.returnUrl);
+                }
+            } else {
+                $scope.loginForm.loginFailure = true;
+            }
+        });
     }
 }
 
-LoginController.$inject = ['$scope', '$routeParams'];
+LoginController.$inject = ['$scope', '$routeParams', '$location', 'LoginFactory'];
